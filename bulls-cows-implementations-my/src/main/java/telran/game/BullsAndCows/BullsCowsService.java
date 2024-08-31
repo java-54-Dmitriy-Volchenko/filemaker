2501c76565cc062a5cfbe1a16ec44d799b8ae320
DmitriyVolchenko <dmitriy.volchenko@gmail.com>
2024-08-31 18:56:40 +0300

    cleans repository

--- bulls-cows-implementations-my/src/main/java/telran/game/BullsAndCows/BullsCowsService.java
+++
@@ -1,9 +0,0 @@
-package telran.game.BullsAndCows;
-
-import java.util.List;
-
-public interface BullsCowsService {
-    Long createNewGame();
-    List<MoveResult> getResults(long gameId, Move move);
-    Boolean isGameOver(long gameId);
-}
